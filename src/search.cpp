eddd05c3b5d0a6c34eb5d46cadb8f2322b044d09
Joona Kiiski <joona.kiiski@gmail.com>
2010-05-22 11:34:35 +0300

    Drop completely illogical ei.kingDanger == 0 requirement
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/search.cpp
+++ src/search.cpp
@@ -1503,7 +1503,7 @@ namespace {
     if (bestValue >= beta)
     {
         // Store the score to avoid a future costly evaluation() call
-        if (!isCheck && !tte && ei.kingDanger[pos.side_to_move()] == 0)
+        if (!isCheck && !tte)
             TT.store(pos.get_key(), value_to_tt(bestValue, ply), VALUE_TYPE_LOWER, Depth(-127*OnePly), MOVE_NONE, ss[ply].eval, ei.kingDanger[pos.side_to_move()]);
 
         return bestValue;
