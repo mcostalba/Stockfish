d9f4f764cdca271878bfbfad56d8b0a2c669a9db
Marco Costalba <mcostalba@gmail.com>
2012-09-21 23:55:33 +0200

    Customize scripts for stockfish
    Needed only once.

--- src/clop-cutechess-cli.py
+++ src/clop-cutechess-cli.py
@@ -13,7 +13,7 @@ Run cutechess-cli with CLOP_PARAM(s).
 CLOP is a black-box parameter tuning tool designed and written by Rémi Coulom.
 More information about CLOP can be found at the CLOP website:
 http://remi.coulom.free.fr/CLOP/
- 
+
 This script works between CLOP and cutechess-cli. The path to this script,
 without any parameters, should be on the "Script" line of the .clop file.
 'Replications' in the .clop file should be set to 2 so that this script can
@@ -36,33 +36,31 @@ import sys
 
 # Path to the cutechess-cli executable.
 # On Windows this should point to cutechess-cli.exe
-cutechess_cli_path = 'path_to_cutechess-cli/cutechess-cli.sh'
+cutechess_cli_path = './cutechess-cli'
 
 # The engine whose parameters will be optimized
-engine = 'conf=MyEngine'
+engine = 'cmd=stockfish'
 
 # Format for the commands that are sent to the engine to
 # set the parameter values. When the command is sent,
 # {name} will be replaced with the parameter name and {value}
 # with the parameter value.
-engine_param_cmd = 'setvalue {name} {value}'
+engine_param_cmd = 'setoption name {name} value {value}'
 
 # A pool of opponents for the engine. The opponent will be
 # chosen based on the seed sent by CLOP.
 opponents = [
-    'conf=OpponentEngine1',
-    'conf=OpponentEngine2',
-    'conf=OpponentEngine3'
+    'cmd=stockfish_master'
 ]
 
 # Additional cutechess-cli options, eg. time control and opening book
-options = '-each tc=40/1+0.05 -draw 80 1 -resign 5 500'
+options = '-both tc=0:5+0.05 proto=uci restart=on option.Threads=1 option.Hash=128 option.Ponder=false option.OwnBook=false dir=.. book=./varied.bin -recover -resign 5 500'
 
 
 def main(argv = None):
     if argv is None:
         argv = sys.argv[1:]
-    
+
     if len(argv) == 0 or argv[0] == '--help':
         sys.stdout.write(__doc__)
         return 0
@@ -71,7 +69,7 @@ def main(argv = None):
     if len(argv) < 3 or len(argv) % 2 == 0:
         sys.stderr.write('Too few arguments\n')
         return 2
-    
+
     clop_seed = 0
     try:
         clop_seed = int(argv[0])
@@ -81,7 +79,7 @@ def main(argv = None):
 
     fcp = engine
     scp = opponents[(clop_seed >> 1) % len(opponents)]
-    
+
     # Parse the parameters that should be optimized
     for i in range(1, len(argv), 2):
         # Make sure the parameter value is numeric
@@ -94,21 +92,21 @@ def main(argv = None):
         # cutechess-cli's initialization string feature
         initstr = engine_param_cmd.format(name = argv[i], value = argv[i + 1])
         fcp += ' initstr="%s"' % initstr
-    
+
     # Choose the engine's playing side (color) based on CLOP's seed
     if clop_seed % 2 != 0:
         fcp, scp = scp, fcp
-    
+
     cutechess_args = '-engine %s -engine %s %s' % (fcp, scp, options)
     command = '%s %s' % (cutechess_cli_path, cutechess_args)
-    
+
     # Run cutechess-cli and wait for it to finish
     process = Popen(command, shell = True, stdout = PIPE)
     output = process.communicate()[0]
     if process.returncode != 0:
         sys.stderr.write('failed to execute command: %s\n' % command)
         return 2
-    
+
     # Convert Cutechess-cli's result into W/L/D
     # Note that only one game should be played
     result = -1
@@ -124,7 +122,7 @@ def main(argv = None):
                 sys.stderr.write('the game did not terminate properly\n')
                 return 2
             break
-    
+
     if result == 0:
         sys.stdout.write('W\n')
     elif result == 1:
