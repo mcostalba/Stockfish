4c128ecea0cf701764c1128075638d8711e2a66f
Gary Linscott <glinscott@gmail.com>
2013-02-05 08:27:18 -0500

    Adjust singular extension pruning by all/cut node type.  Bench: 3470580

--- src/thread.h
+++ src/thread.h
@@ -68,6 +68,7 @@ struct SplitPoint {
   Value beta;
   int nodeType;
   Move threatMove;
+  bool allNode;
 
   // Const pointers to shared data
   MovePicker* movePicker;
@@ -104,7 +105,7 @@ struct Thread {
 
   template <bool Fake>
   void split(Position& pos, Search::Stack* ss, Value alpha, Value beta, Value* bestValue, Move* bestMove,
-             Depth depth, Move threatMove, int moveCount, MovePicker* movePicker, int nodeType);
+             Depth depth, Move threatMove, int moveCount, MovePicker* movePicker, int nodeType, bool allNode);
 
   SplitPoint splitPoints[MAX_SPLITPOINTS_PER_THREAD];
   Material::Table materialTable;
