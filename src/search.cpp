2087d663a20d2652f092d9ac450a62e43c7f313d
Lucas Braesch <lucas.braesch@gmail.com>
2013-02-13 18:46:20 +0800

    Retire non capturing evasion pruning in qsearch() Based on my experience in DiscoCheck, I know that this is at least useless, and most likely harmful. Same for the PvNode condition, which doesn't make any sense. Confirmed intesting:
    6000 games in 10"+0.1"
    1299-1221-3480 [50.6%] LOS=94.0%
    
    signature=5168496

--- src/search.cpp
+++ src/search.cpp
@@ -1115,7 +1115,7 @@ split_point_start: // At split points actual search starts from here
     Key posKey;
     Move ttMove, move, bestMove;
     Value bestValue, value, ttValue, futilityValue, futilityBase, oldAlpha;
-    bool givesCheck, enoughMaterial, evasionPrunable;
+    bool givesCheck, enoughMaterial;
     Depth ttDepth;
 
     // To flag BOUND_EXACT a node with eval above alpha and no available moves
@@ -1231,17 +1231,8 @@ split_point_start: // At split points actual search starts from here
           }
       }
 
-      // Detect non-capture evasions that are candidate to be pruned
-      evasionPrunable =   !PvNode
-                       &&  InCheck
-                       &&  bestValue > VALUE_MATED_IN_MAX_PLY
-                       && !pos.is_capture(move)
-                       && !pos.can_castle(pos.side_to_move());
-
       // Don't search moves with negative SEE values
-      if (   !PvNode
-          && (!InCheck || evasionPrunable)
-          &&  move != ttMove
+      if ( !InCheck
           &&  type_of(move) != PROMOTION
           &&  pos.see_sign(move) < 0)
           continue;
