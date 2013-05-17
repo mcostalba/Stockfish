bad495c37e62455749b291f18b0483021b46c637
Marco Costalba <mcostalba@gmail.com>
2013-05-17 07:14:13 +0200

    Don't reduce countermoves
    As we do for killers. Only about 5% of moves are
    affected.
    
    bench: 4823653

--- src/search.cpp
+++ src/search.cpp
@@ -947,7 +947,9 @@ split_point_start: // At split points actual search starts from here
           && !dangerous
           &&  move != ttMove
           &&  move != ss->killers[0]
-          &&  move != ss->killers[1])
+          &&  move != ss->killers[1]
+          &&  move != countermoves[0]
+          &&  move != countermoves[1])
       {
           ss->reduction = reduction<PvNode>(depth, moveCount);
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
