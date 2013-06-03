ed8d089a7bff10dba6d839de65bd58a2d3d1e08f
Marco Costalba <mcostalba@gmail.com>
2013-06-03 10:48:20 +0200

    Decrease Cowardice of -20
    Decreasing -10 failed but with score above 50%
    
    bench: 4388609

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -102,7 +102,7 @@ namespace {
   //
   // Values modified by Joona Kiiski
   const Score WeightsInternal[] = {
-      S(289, 344), S(233, 201), S(261, 313), S(46, 0), S(271, 0), S(307, 0)
+      S(289, 344), S(233, 201), S(221, 273), S(46, 0), S(251, 0), S(307, 0)
   };
 
   // MobilityBonus[PieceType][attacked] contains bonuses for middle and end
