c46ac44525445cabaf4fd3941ef9e06bee23851c
Marco Costalba <mcostalba@gmail.com>
2013-06-13 08:08:27 +0200

    Don't reduce more countermoves
    bench: 4604805

--- src/search.cpp
+++ src/search.cpp
@@ -949,11 +949,11 @@ split_point_start: // At split points actual search starts from here
       {
           ss->reduction = reduction<PvNode>(depth, moveCount);
 
-          if (!PvNode && cutNode)
-              ss->reduction += 2 * ONE_PLY;
-
           if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
+
+          else if (!PvNode && cutNode)
+              ss->reduction += ONE_PLY;
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
