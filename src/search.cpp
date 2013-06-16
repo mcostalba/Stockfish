dcf017ba8039c2cf8e68cebc8b02f2de477d1eb7
Marco Costalba <mcostalba@gmail.com>
2013-06-16 09:17:38 +0200

    Don't overlap with the LMR cut reduction
    bench: 3271364

--- src/search.cpp
+++ src/search.cpp
@@ -951,9 +951,6 @@ split_point_start: // At split points actual search starts from here
       {
           ss->reduction = reduction<PvNode>(depth, moveCount);
 
-          if (!PvNode && cutNode)
-              ss->reduction += ONE_PLY;
-
           if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
 
