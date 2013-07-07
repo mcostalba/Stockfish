7e457e32fee220db427f07783455c118176b412d
Marco Costalba <mcostalba@gmail.com>
2013-07-07 12:52:18 +0200

    Revert "Increased static null pruning"
    Actually what was going on is exactly the opposite.
    
    In case parent node is deep in move list then
    
    futility_margin(depth, (ss-1)->futilityMoveCount)
    
    Is higher and
    
    eval - futility_margin(depth, (ss-1)->futilityMoveCount)
    
    Is smaller so that it is less probable that can reach beta.
    
    bench: 4432633

--- src/search.cpp
+++ src/search.cpp
@@ -639,11 +639,11 @@ namespace {
         && !ss->skipNullMove
         &&  depth < 4 * ONE_PLY
         && !inCheck
-        &&  eval - futility_margin(depth, (ss-1)->futilityMoveCount) >= beta
+        &&  eval - futility_margin(depth, 0) >= beta
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY
         &&  abs(eval) < VALUE_KNOWN_WIN
         &&  pos.non_pawn_material(pos.side_to_move()))
-        return eval - futility_margin(depth, (ss-1)->futilityMoveCount);
+        return eval - futility_margin(depth, 0);
 
     // Step 8. Null move search with verification search (is omitted in PV nodes)
     if (   !PvNode
