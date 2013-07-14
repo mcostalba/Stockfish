4cfd86b883536192ff75d48bce582d822c26709b
Marco Costalba <mcostalba@gmail.com>
2013-07-14 23:37:11 +0200

    BishopPin disabled
    bench: 4545592

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -165,7 +165,7 @@ namespace {
   #undef S
 
   const Score Tempo            = make_score(24, 11);
-  const Score BishopPin        = make_score(36, 11);
+  const Score BishopPin        = make_score( 0,  0);
   const Score RookOn7th        = make_score(11, 20);
   const Score QueenOn7th       = make_score( 3,  8);
   const Score RookOnPawn       = make_score(10, 28);
