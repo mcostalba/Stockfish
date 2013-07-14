83ab846ef6e663b7c3178de3b4e725b2f7a087af
Marco Costalba <mcostalba@gmail.com>
2013-07-14 13:19:33 +0200

    RookOnPawn (+6 + 10)
    bench: 5038533

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -167,8 +167,8 @@ namespace {
   const Score Tempo            = make_score(24, 11);
   const Score BishopPin        = make_score(66, 11);
   const Score RookOn7th        = make_score(11, 20);
-  const Score QueenOn7th       = make_score( 0,  0);
-  const Score RookOnPawn       = make_score(10, 28);
+  const Score QueenOn7th       = make_score( 3,  8);
+  const Score RookOnPawn       = make_score(16, 38);
   const Score QueenOnPawn      = make_score( 4, 20);
   const Score RookOpenFile     = make_score(43, 21);
   const Score RookSemiopenFile = make_score(19, 10);
