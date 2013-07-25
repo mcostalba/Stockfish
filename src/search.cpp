fd5c1b2ea804e0075d02bc03d4d2e7acdda82162
Eelco de Groot <Kingdefendergit@gmail.com>
2013-07-26 01:31:05 +0200

    Search change

--- src/search.cpp
+++ src/search.cpp
@@ -869,6 +869,17 @@ moves_loop: // When in check and at SpNode search starts from here
 
               continue;
           }
+          
+          // Quiet move count and value based pruning
+          if (    depth < 16 * ONE_PLY
+              &&  cutNode
+              && !SpNode
+              &&  ss->staticEval + ss->evalMargin < (ss-2)->staticEval + (ss-2)->evalMargin
+              &&  moveCount + quietCount >= FutilityMoveCounts[depth]
+              &&  (!threatMove || !refutes(pos, move, threatMove)))
+          {
+              continue;
+          }
 
           // Value based pruning
           // We illogically ignore reduction condition depth >= 3*ONE_PLY for predicted depth,
