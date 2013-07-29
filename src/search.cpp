c13da4dcc5fa237c019a0cba20b7fb81280f79a2
Marco Costalba <mcostalba@gmail.com>
2013-07-29 10:39:50 +0200

    Wider search in SMP case
    Same signature becuase bench is run in single core,
    but functionality is affected in SMP case.
    
    bench: 4364793

--- src/search.cpp
+++ src/search.cpp
@@ -942,8 +942,11 @@ moves_loop: // When in check and at SpNode search starts from here
           if (!PvNode && cutNode)
               ss->reduction += ONE_PLY;
 
+          if (SpNode)
+              ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
+
           if (move == countermoves[0] || move == countermoves[1])
-              ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
+              ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
