f79be7bd4a6a7788062a49639bbdb19091cde246
Marco Costalba <mcostalba@gmail.com>
2013-10-25 09:18:08 +0200

    Imbalance: tune pawn (take 4)
    bench: 9171356

--- src/material.cpp
+++ src/material.cpp
@@ -39,7 +39,7 @@ namespace {
   const Value RedundantRook  = Value(554);
 
   //                                  pair  pawn knight bishop rook queen
-  const int LinearCoefficients[6] = { 1617, -182, -1172, -190,  105,  26 };
+  const int LinearCoefficients[6] = { 1617, -202, -1172, -190,  105,  26 };
 
   const int QuadraticCoefficientsSameColor[][PIECE_TYPE_NB] = {
     // pair pawn knight bishop rook queen
