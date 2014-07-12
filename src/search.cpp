24ea021c10d7ae48e7662ee940b7f4f94cac4e28
Marco Costalba <mcostalba@gmail.com>
2014-07-12 09:31:29 +0200

    Better document singularExtensionNode condition
    Picked from official branch.
    
    No functional change.

--- src/search.cpp
+++ src/search.cpp
@@ -697,6 +697,7 @@ moves_loop: // When in check and at SpNode search starts from here
                            &&  depth >= 8 * ONE_PLY
                            &&  abs(beta) < VALUE_KNOWN_WIN
                            &&  ttMove != MOVE_NONE
+                       /*  &&  ttValue != VALUE_NONE Already implicit in the next condition */
                            &&  abs(ttValue) < VALUE_KNOWN_WIN
                            && !excludedMove // Recursive singular search is not allowed
                            && (tte->bound() & BOUND_LOWER)
