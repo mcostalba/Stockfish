c18f9c8fd50d3fb3a3fdcc9a8ac46e612adcd6ae
Marco Costalba <mcostalba@gmail.com>
2015-01-05 09:20:36 +0100

    Remove useless rule in passed pawns eval
    Tuning of this part of code seems to hint
    that this rule is useless.
    
    bench: 8409246

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -580,10 +580,6 @@ namespace {
             ebonus +=  distance(pos.king_square(Them), blockSq) * 5 * rr
                      - distance(pos.king_square(Us  ), blockSq) * 2 * rr;
 
-            // If blockSq is not the queening square then consider also a second push
-            if (relative_rank(Us, blockSq) != RANK_8)
-                ebonus -= distance(pos.king_square(Us), blockSq + pawn_push(Us)) * rr;
-
             // If the pawn is free to advance, then increase the bonus
             if (pos.empty(blockSq))
             {
