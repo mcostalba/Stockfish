d40d0ce55d8bcd2cb275132465b8478775395f8f
Marco Costalba <mcostalba@gmail.com>
2015-04-04 08:54:15 +0200

    Fishtest Tuning Framework
    The purpose of the code is to allow developers to easily and flexibly
    setup SF for a tuning session. Mainly you have just to remove 'const'
    qualifiers from the variables you want to tune and flag them for
    tuning, so if you have:
    
    int myKing = 10;
    Score myBonus = S(5, 15);
    Value myValue[][2] = { { V(100), V(20) }, { V(7), V(78) } };
    
    and at the end of the update you may want to call
    a post update function:
    
    void my_post_update();
    
    If instead of default Option's min-max values,
    you prefer your custom ones, returned by:
    
    std::pair<int, int> my_range(int value)
    
    Or you jus want to set the range directly, you can
    simply add below:
    
    TUNE(SetRange(my_range), myKing, SetRange(-200, 200), myBonus, myValue, my_post_update);
    
    And all the magic happens :-)
    
    At startup all the parameters are printed in a
    format suitable to be copy-pasted in fishtest.
    
    No functional change.
    On branch tune
    	modified:   src/evaluate.cpp
    Untracked files:
    	FishBench.exe
    no changes added to commit (use "git add" and/or "git commit -a")
    On branch tune
    	modified:   src/evaluate.cpp
    Untracked files:
    	FishBench.exe
    no changes added to commit (use "git add" and/or "git commit -a")
    On branch tune
    	modified:   src/evaluate.cpp
    Untracked files:
    	FishBench.exe
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -161,3 +161,99 @@ Option& Option::operator=(const string& v) {
 }
 
 } // namespace UCI
+
+
+
+/// Tuning Framework. Fully separated from SF code, appended here to avoid
+/// adding a *.cpp file and to modify Makefile.
+
+#include <iostream>
+#include <sstream>
+
+static std::map<std::string, int> TuneResults;
+
+string Tune::next(string& names, bool pop) {
+
+  string name;
+
+  do {
+      string token = names.substr(0, names.find(','));
+
+      if (pop)
+          names.erase(0, token.size() + 1);
+
+      std::stringstream ws(token);
+      name += (ws >> token, token); // Remove trailing whitespace
+
+  } while (  std::count(name.begin(), name.end(), '(')
+           - std::count(name.begin(), name.end(), ')'));
+
+  return name;
+}
+
+static void on_tune(const UCI::Option&) { Tune::read_options(); }
+
+static void make_option(const string& n, int v, const SetRange& r) {
+
+  // Do not generate option when there is nothing to tune (ie. min = max)
+  if (r(v).first == r(v).second)
+      return;
+
+  if (TuneResults.count(n))
+      v = TuneResults[n];
+
+  Options[n] << UCI::Option(v, r(v).first, r(v).second, on_tune);
+
+  // Print formatted parameters, ready to be copy-pasted in fishtest
+  std::cout << n << ","
+            << v << ","
+            << r(v).first << "," << r(v).second << ","
+            << (r(v).second - r(v).first) / 20.0 << ","
+            << "0.0020"
+            << std::endl;
+}
+
+template<> void Tune::Entry<int>::init_option() { make_option(name, value, range); }
+
+template<> void Tune::Entry<int>::read_option() {
+  if (Options.count(name))
+      value = Options[name];
+}
+
+template<> void Tune::Entry<Value>::init_option() { make_option(name, value, range); }
+
+template<> void Tune::Entry<Value>::read_option() {
+  if (Options.count(name))
+      value = Value(int(Options[name]));
+}
+
+template<> void Tune::Entry<Score>::init_option() {
+  make_option("m" + name, mg_value(value), range);
+  make_option("e" + name, eg_value(value), range);
+}
+
+template<> void Tune::Entry<Score>::read_option() {
+  if (Options.count("m" + name))
+      value = make_score(Options["m" + name], eg_value(value));
+
+  if (Options.count("e" + name))
+      value = make_score(mg_value(value), Options["e" + name]);
+}
+
+// Instead of a variable here we have a PostUpdate function: just call it
+template<> void Tune::Entry<Tune::PostUpdate>::init_option() {}
+template<> void Tune::Entry<Tune::PostUpdate>::read_option() { value(); }
+
+// Init options with tuning session results instead of default values. Useful to
+// get correct bench signature after a tuning session or to test tuned values.
+// Just copy fishtest tuning results in a tune_result.txt file and extract the
+// values with:
+//
+//  cat tune_result.txt | sed 's/^param: \([^,]*\), best: \([^,]*\).*/  TuneResults["\1"] = int(\2);/'
+//
+// Then paste the output below, as the function body
+
+void Tune::read_results() {
+
+  /* ...insert your values here... */
+}
