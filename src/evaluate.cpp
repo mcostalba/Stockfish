7adf788d1beaa81e98b04367816eb7602955742c
ianfab <ianfab@users.noreply.github.com>
2016-07-27 20:39:13 +0200

    Remove Racing Kings king safety bonus/penalty
    ELO: 143.89 +-22.3 (95%) LOS: 100.0%
    Total: 1000 W: 656 L: 264 D: 80

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -402,6 +402,12 @@ namespace {
 
     // King shelter and enemy pawns storm
     Score score = ei.pi->king_safety<Us>(pos, ksq);
+#ifdef RACE
+    if (pos.is_race())
+        score = make_score(0, 0);
+    else
+    {
+#endif
 
     // Main king safety evaluation
     if (ei.kingAttackersCount[Them])
@@ -499,8 +505,7 @@ namespace {
         score -= KingDanger[std::max(std::min(attackUnits, 399), 0)];
     }
 #ifdef RACE
-    if (pos.is_race())
-        score = -score;
+    }
 #endif
 
     if (DoTrace)
