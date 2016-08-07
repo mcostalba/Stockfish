ba74d6c61456c7269e34262ff2d74214eedbd3f4
ianfab <ianfab@users.noreply.github.com>
2016-08-07 13:39:19 +0200

    Change move ordering in Racing Kings
    ELO: 40.48 +-19.5 (95%) LOS: 100.0%
    Total: 1000 W: 464 L: 348 D: 188

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -134,6 +134,11 @@ void MovePicker::score<CAPTURES>() {
   // badCaptures[] array, but instead of doing it now we delay until the move
   // has been picked up, saving some SEE calls in case we get a cutoff.
   for (auto& m : *this)
+#ifdef RACE
+      if (pos.is_race())
+          m.value = PieceValue[MG][pos.piece_on(to_sq(m))] - Value(200 * relative_rank(BLACK, to_sq(m)));
+      else
+#endif
       m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
                - Value(200 * relative_rank(pos.side_to_move(), to_sq(m)));
 }
