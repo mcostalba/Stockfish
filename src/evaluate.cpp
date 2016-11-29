503a9ed202e5b76ca9fa4c41567bc8f164babb83
Marco Costalba <mcostalba@gmail.com>
2016-11-29 10:12:31 +0100

    Tweak king safety threashold: take 1
    bench: 5342584

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -237,7 +237,7 @@ namespace {
     ei.attackedBy2[Us] = ei.attackedBy[Us][PAWN] & ei.attackedBy[Us][KING];
 
     // Init king safety tables only if we are going to use them
-    if (pos.non_pawn_material(Us) >= QueenValueMg)
+    if (pos.non_pawn_material(Us) >= 3 * QueenValueMg / 4)
     {
         ei.kingRing[Them] = b | shift<Down>(b);
         b &= ei.attackedBy[Us][PAWN];
