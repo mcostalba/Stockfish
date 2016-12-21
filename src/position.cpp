b285f6a1b0f3429aa475166a42cb239b8fb94ae0
Daniel Dugovic <dandydand@gmail.com>
2016-12-21 09:36:32 -0600

    During setup add in-hand pieces to nonPawnMaterial #141

--- src/position.cpp
+++ src/position.cpp
@@ -589,7 +589,10 @@ void Position::set_state(StateInfo* si) const {
 
 #ifdef CRAZYHOUSE
       if (is_house())
+      {
+          si->nonPawnMaterial[color_of(pc)] += pieceCountInHand[color_of(pc)][type_of(pc)] * PieceValue[CHESS_VARIANT][MG][pc];
           si->key ^= Zobrist::inHand[pc][pieceCountInHand[color_of(pc)][type_of(pc)]];
+      }
 #endif
   }
 
