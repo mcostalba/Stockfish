c8aebb5fea2668de05d3b5d991db4fb3a2501d23
Daniel Dugovic <dandydand@gmail.com>
2020-08-15 08:45:22 -0500

    Use classical (no NNUE) evaluation for two kings

--- src/position.cpp
+++ src/position.cpp
@@ -250,9 +250,6 @@ Position& Position::set(const string& fenStr, bool isChess960, Variant v, StateI
           {
               // Kings get a fixed ID, other pieces get ID in order of placement
               piece_id =
-#ifdef TWOKINGS
-                is_two_kings() ? next_piece_id++ :
-#endif
                 (idx == W_KING) ? PIECE_ID_WKING :
                 (idx == B_KING) ? PIECE_ID_BKING :
                 next_piece_id++;
