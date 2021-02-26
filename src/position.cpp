33ab053d119eb5f7e4a6023356236a6e67003ce6
Daniel Dugovic <dandydand@gmail.com>
2021-02-26 12:24:21 -0600

    Attempt to fix atomic en passant

--- src/position.cpp
+++ src/position.cpp
@@ -376,18 +376,23 @@ Position& Position::set(const string& fenStr, bool isChess960, Variant v, StateI
   if (enpassant) {
       st->previous = new StateInfo();
       remove_piece(st->epSquare - pawn_push(sideToMove));
+#ifdef ATOMIC
+      if (is_atomic() && (is_atomic_loss() || kings_adjacent())) {
+          st->previous->checkersBB = 0;
+          st->previous->blockersForKing[WHITE] = st->previous->blockersForKing[BLACK] = 0;
+      } else
+#endif
 #ifdef ANTI
       if (is_anti()) {
           st->previous->checkersBB = 0;
-          si->previous->blockersForKing[WHITE] = si->previous->blockersForKing[BLACK] = 0;
-      } else {
+          st->previous->blockersForKing[WHITE] = st->previous->blockersForKing[BLACK] = 0;
+      } else
 #endif
+      {
       st->previous->checkersBB = attackers_to(square<KING>(~sideToMove)) & pieces(sideToMove);
       st->previous->blockersForKing[WHITE] = slider_blockers(pieces(BLACK), square<KING>(WHITE), st->previous->pinners[BLACK]);
       st->previous->blockersForKing[BLACK] = slider_blockers(pieces(WHITE), square<KING>(BLACK), st->previous->pinners[WHITE]);
-#ifdef ANTI
       }
-#endif
       put_piece(make_piece(~sideToMove, PAWN), st->epSquare - pawn_push(sideToMove));
   }
   else
